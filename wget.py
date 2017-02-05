6afc86e50a829466c5c435cb797bbcab1c3fe961
fanxuanhui <750657961@qq.com>
2017-02-05 13:02:02 +0800

    Update wget.py

--- wget.py
+++ wget.py
@@ -4,7 +4,7 @@ import urllib
 import urllib2
 import sys
 file_dir=sys.argv[1]
-#file_dir="http://121.42.183.93:81/all.tar.gz"
+#file_dir="http://111.42.183.93:181/all.tar.gz"
 data=urllib2.urlopen(file_dir).read().strip()
 #print(data)
 with open("all.tar.gz","w") as fd:
